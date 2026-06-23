274c21e29df1d42ef1357d453ef24cef82f3835e
Cory Caron <corycaron5@gmail.com>
2026-06-23 12:51:49 -0400

    CheatCore initial commit

---
+++ OvaCheatTool/CheatCore.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(OvaCheatTool.CheatCore), "OvaCheatTool", "1.0.0", "Cory", null)]
+[assembly: MelonGame("Skinny Frog", "Ova Magica")]
+
+namespace OvaCheatTool
+{
+    public class CheatCore : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
